14cd11d7cf7903c16afee9b7a604450e00f08343
Brishketu Kislay <brishketu.kislay@gmail.com>
2024-01-03 05:10:03 +0000

    router change

--- app/users/Header/header.tsx
+++ app/users/Header/header.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect, useState } from 'react';
 import style from './header.module.css';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 const Header = () => {
   const router = useRouter();
