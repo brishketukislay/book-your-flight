b68d8dc3ae49e22b3c8791b2578d252b0fdea8ba
Brishketu Kislay <brishketu.kislay@gmail.com>
2024-01-03 02:41:12 +0000

    route update

--- app/users/NavBar/navBar.tsx
+++ app/users/NavBar/navBar.tsx
@@ -14,7 +14,7 @@ const NavBar = () => {
         <div className={pathname == '/users/travel-planner'?`${style.navElement} ${style.active}`:style.navElement }>
             <Link href='/users/travel-planner'>Travel Planner</Link>
         </div>
-        {/* <div className={pathname == '/users/feedback'?`${style.navElement} ${style.active}`:style.navElement }>
+         <div className={pathname == '/users/feedback'?`${style.navElement} ${style.active}`:style.navElement }>
                 <Link href='/users/feedback'>Feedback</Link>
             </div>
             <div className={pathname == '/users/support'?`${style.navElement} ${style.active}`:style.navElement }>
@@ -22,7 +22,7 @@ const NavBar = () => {
             </div>
             <div className={pathname == '/users/sharing-collabs'?`${style.navElement} ${style.active}`:style.navElement }>
                 <Link href='/users/sharing-collabs'>Sharing & Collabs</Link>
-            </div> */}
+            </div> 
     </div>
   )
 }
