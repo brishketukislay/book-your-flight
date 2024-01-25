cd7e352d20db9d9479a303244031003aaa791562
Brishketu Kislay <brishketu.kislay@gmail.com>
2024-01-25 03:57:08 +0000

    responsiveness update

--- app/users/NavBar/navBar.tsx
+++ app/users/NavBar/navBar.tsx
@@ -3,18 +3,29 @@ import React from 'react'
 import style from './nav-bar.module.css'
 import { usePathname } from "next/navigation";
 import Link from 'next/link';
+import SvgIcon from '@mui/material/SvgIcon';
+import HomeIcon from '@mui/icons-material/Home';
+import FlightIcon from '@mui/icons-material/Flight';
 
 const NavBar = () => {
     const pathname = usePathname();
-  return (
-    <div className={style.sideNav}>
-        <div className={pathname == '/users'?`${style.navElement} ${style.active}`:style.navElement }>
-           <Link href='/users'>Dashboard</Link>
-        </div>
-        <div className={pathname == '/users/travel-planner'?`${style.navElement} ${style.active}`:style.navElement }>
-            <Link href='/users/travel-planner'>Travel Planner</Link>
-        </div>
-        {/* <div className={pathname == '/users/feedback'?`${style.navElement} ${style.active}`:style.navElement }>
+
+    return (
+        <div className={style.sideNav}>
+            {/* Dashboard Link */}
+            <div className={pathname === '/users' ? `${style.navElement} ${style.active}` : style.navElement}>
+                <Link href='/users'>
+                    <p className={style.tabStyle}>Dashboard</p><SvgIcon className={style.icons} component={HomeIcon} />
+                </Link>
+            </div>
+
+            {/* Travel Planner Link */}
+            <div className={pathname === '/users/travel-planner' ? `${style.navElement} ${style.active}` : style.navElement}>
+                <Link href='/users/travel-planner'><p className={style.tabStyle}>Travel Planner</p>
+                <SvgIcon className={style.icons} component={FlightIcon} /></Link>
+            </div>
+
+                    {/* <div className={pathname == '/users/feedback'?`${style.navElement} ${style.active}`:style.navElement }>
             <Link href='/users/feedback'>Feedback</Link>
         </div>
         <div className={pathname == '/users/support'?`${style.navElement} ${style.active}`:style.navElement }>
@@ -23,8 +34,8 @@ const NavBar = () => {
         <div className={pathname == '/users/sharing-collabs'?`${style.navElement} ${style.active}`:style.navElement }>
             <Link href='/users/sharing-collabs'>Sharing & Collabs</Link>
         </div> */}
-    </div>
-  )
+        </div>
+    )
 }
 
 export default NavBar
